435b051ca459e1463362118ef43ae2891c7142e0
Jordan Pham <98629983+Jordanpham938@users.noreply.github.com>
2024-02-18 22:41:24 -0800

    Update variable.js
    Signed-off-by: Jordan Pham <98629983+Jordanpham938@users.noreply.github.com>

--- variable.js
+++ variable.js
@@ -1,16 +1,12 @@
 // TODO: Create a variable named `firstName` and assign your first name to it in the line below.
-let firstName = 'Meredith';
+let firstName = 'Jordan';
 
 // TODO: Create a variable named `lastName` and assign your last name to it in the line below.
-let lastName = 'Gatlin';
-
+let lastName = 'Pham';
 
 // TODO: Create a variable named `sayHello` and build the following string based on the variables
 // above: "Hello world! My name is <firstName> <lastName>. Nice to meet you!". 
-let sayHello = 'Hello World! My name is,';
-let nice = 'Nice to meet you!';
-let greeting = `${sayHello} ${firstName} ${lastName}. ${nice}`;
-
+let sayHello = `Hello world! My name is ${firstName} ${lastName}. Nice to meet you!`;
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
 
